90cc31ed3ebbfd9938a419a774a96251b88f7106
MohamedAshraf132 <ma5257112@gmail.com>
2025-02-24 16:34:40 +0200

    splash screen

---
+++ lib/views/widgets/SplashScren_Body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SplashscrenBody extends StatelessWidget {
+  const SplashscrenBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}

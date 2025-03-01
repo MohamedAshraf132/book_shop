e722cae94bcc5b8f0eaf354f549ec981b7a0d875
MohamedAshraf132 <ma5257112@gmail.com>
2025-03-01 13:51:35 +0200

    refactor init state

--- lib/views/widgets/SplashScren_Body.dart
+++ lib/views/widgets/SplashScren_Body.dart
@@ -1,4 +1,6 @@
+import 'package:book_shop/views/homeScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SplashScreenBody extends StatefulWidget {
   const SplashScreenBody({super.key});
@@ -13,6 +15,18 @@ class _SplashScreenBodyState extends State<SplashScreenBody> {
   @override
   void initState() {
     super.initState();
+    initAnimation();
+    navigateToHome();
+  }
+
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.off(() => const HomeScreen(),
+          transition: Transition.fadeIn, duration: const Duration(seconds: 1));
+    });
+  }
+
+  void initAnimation() {
     Future.delayed(const Duration(milliseconds: 500), () {
       setState(() {
         _opacity = 1.0;
@@ -22,27 +36,29 @@ class _SplashScreenBodyState extends State<SplashScreenBody> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          AnimatedOpacity(
-            duration: const Duration(seconds: 2),
-            curve: Curves.easeInOut,
-            opacity: _opacity,
-            child: Image.asset('assets/images/Logo.png', height: 100),
-          ),
-          const SizedBox(height: 4),
-          AnimatedOpacity(
-            duration: const Duration(seconds: 2),
-            curve: Curves.easeInOut,
-            opacity: _opacity,
-            child: const Text(
-              'Read Free Books',
-              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedOpacity(
+              duration: const Duration(seconds: 2),
+              curve: Curves.easeInOut,
+              opacity: _opacity,
+              child: Image.asset('assets/images/Logo.png', height: 100),
             ),
-          ),
-        ],
+            const SizedBox(height: 4),
+            AnimatedOpacity(
+              duration: const Duration(seconds: 2),
+              curve: Curves.easeInOut,
+              opacity: _opacity,
+              child: const Text(
+                'Read Free Books',
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
